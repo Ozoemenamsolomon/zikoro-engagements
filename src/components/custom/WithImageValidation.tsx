bdb7f90fbc51bff925438f517fda85fe72afa214
sekani50 <mohammedadeoye0@gmail.com>
2025-01-22 07:05:42 -0800

    fix: commented out the image validator

--- src/components/custom/WithImageValidation.tsx
+++ src/components/custom/WithImageValidation.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { isImageValid } from "@/utils";
+// import React from "react";
+// import { isImageValid } from "@/utils";
 
-interface WithImageValidationProps {
-  src: string;
-  alt: string;
-  className: string;
-  width: number;
-  height: number;
-}
+// interface WithImageValidationProps {
+//   src: string;
+//   alt: string;
+//   className: string;
+//   width: number;
+//   height: number;
+// }
 
-const withImageValidation = <P extends object>(
-  Component: React.ComponentType<P & { isValid: boolean }>
-) => {
-  return async (props: Omit<P, "isValid"> & WithImageValidationProps) => {
-    const isValid = await isImageValid(props.src);
-    return <Component {...(props as P)} isValid={isValid} />;
-  };
-};
+// const withImageValidation = <P extends object>(
+//   Component: React.ComponentType<P & { isValid: boolean }>
+// ) => {
+//   return async (props: Omit<P, "isValid"> & WithImageValidationProps) => {
+//     const isValid = await isImageValid(props.src);
+//     return <Component {...(props as P)} isValid={isValid} />;
+//   };
+// };
 
-export default withImageValidation;
+// export default withImageValidation;
