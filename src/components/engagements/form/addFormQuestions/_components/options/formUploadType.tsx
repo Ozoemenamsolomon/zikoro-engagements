bf8b4604df65c65fb98ae197a43d7c503082814d
sekani50 <mohammedadeoye0@gmail.com>
2025-02-04 12:56:42 -0800

    jhv

--- src/components/engagements/form/addFormQuestions/_components/options/formUploadType.tsx
+++ src/components/engagements/form/addFormQuestions/_components/options/formUploadType.tsx
@@ -39,6 +39,8 @@ export function FormUploadType({
     }
   }, [addedImage]);
 
+  
+
   const defaultDescriptionValue = useMemo(() => {
     if (typeof addedDescription === "string" && addedDescription?.length > 0) {
       return addedDescription;
--- src/components/engagements/qa/organizer/_components/QaAd.tsx
+++ src/components/engagements/qa/organizer/_components/QaAd.tsx
@@ -25,7 +25,7 @@ export function QaAdvert({
   closeMobile:() => void;
 }) {
  // console.log("ileft", isLeftBox, isRightBox);
-  const qaLink = `https://engagement.zikoro.com/e/${qa?.workspaceAlias}/qa/a/${qa?.QandAAlias}`
+  const qaLink = `https://engagements.zikoro.com/e/${qa?.workspaceAlias}/qa/a/${qa?.QandAAlias}`
   const [isCopy, setCopy] = useState(false)
   return (
     <div
