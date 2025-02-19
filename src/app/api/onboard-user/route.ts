462ab92fc93a882bc1b8a3c4ab84628e82d68fa1
sekani50 <mohammedadeoye0@gmail.com>
2025-02-19 09:38:02 -0800

    ibubui

--- src/app/api/onboard-user/route.ts
+++ src/app/api/onboard-user/route.ts
@@ -6,52 +6,63 @@ export async function POST(req: NextRequest) {
 
   if (req.method === "POST") {
     try {
-      // Query users who registered 2 hours ago
-      const twoMinutesAgo = new Date(new Date().getTime() - 2 * 60 * 1000).toISOString();
-      const oneSecondBeforeTwoMinutesAgo = new Date(new Date().getTime() - (2 * 60 * 1000) - 1000).toISOString();
+      // Query users who registered 2 minutes ago
+      const twoMinutesAgo = new Date(
+        new Date().getTime() - 2 * 60 * 1000
+      ).toISOString();
+      const oneSecondBeforeTwoMinutesAgo = new Date(
+        new Date().getTime() - 2 * 60 * 1000 - 1000
+      ).toISOString();
 
       const { data: users, error } = await supabase
-        .from('users')
-        .select('*')
-        .gte('created_at', oneSecondBeforeTwoMinutesAgo)
-        .lte('created_at', twoMinutesAgo);
-       // .eq('welcome_email_sent', false); // Assuming you have a column to track if the email was sent
+        .from("users")
+        .select("*")
+        .gte("created_at", oneSecondBeforeTwoMinutesAgo)
+        .lte("created_at", twoMinutesAgo);
+      // .eq('welcome_email_sent', false); // Assuming you have a column to track if the email was sent
 
       if (error) {
         throw error;
       }
 
-      var { SendMailClient } = require("zeptomail");
+      // var { SendMailClient } = require("zeptomail");
 
-      let client = new SendMailClient({
-        url: process.env.NEXT_PUBLIC_ZEPTO_URL,
-        token: process.env.NEXT_PUBLIC_ZEPTO_TOKEN,
-      });
+      // let client = new SendMailClient({
+      //   url: process.env.NEXT_PUBLIC_ZEPTO_URL,
+      //   token: process.env.NEXT_PUBLIC_ZEPTO_TOKEN,
+      // });
 
       // Send email to each user
       for (const user of users) {
-        const resp = await client.sendMail({
-          from: {
-            address: process.env.NEXT_PUBLIC_EMAIL,
-            name: "Zikoro",
-          },
-          to: [
-            {
-              email_address: {
-                address: user.email,
-                name: user.name,
-              },
-            },
-          ],
-          subject: `Welcome to Zikoro`,
-          htmlbody: `<div>Welcome to Zikoro</div>`,
-        });
+        const { error } = await supabase
+          .from("quiz")
+          .upsert({
+            coverImage: "",
+            title: user.email,
+            description: "Hello there",
+          });
+        // const resp = await client.sendMail({
+        //   from: {
+        //     address: process.env.NEXT_PUBLIC_EMAIL,
+        //     name: "Zikoro",
+        //   },
+        //   to: [
+        //     {
+        //       email_address: {
+        //         address: user.email,
+        //         name: user.name,
+        //       },
+        //     },
+        //   ],
+        //   subject: `Welcome to Zikoro`,
+        //   htmlbody: `<div>Welcome to Zikoro</div>`,
+        // });
 
-        // Mark the email as sent
-        await supabase
-          .from('users')
-          .update({ welcome_email_sent: true })
-          .eq('id', user.id);
+        // // Mark the email as sent
+        // await supabase
+        //   .from('users')
+        //   .update({ welcome_email_sent: true })
+        //   .eq('id', user.id);
       }
 
       // success - Looks Good
@@ -62,7 +73,6 @@ export async function POST(req: NextRequest) {
         }
       );
     } catch (error) {
-
       // intentionally return 500
       return NextResponse.json(
         {
