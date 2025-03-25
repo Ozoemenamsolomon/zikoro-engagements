750b13d7d8f7cac9916cdbaec21a6c676e70113e
sekani50 <mohammedadeoye0@gmail.com>
2025-03-25 08:21:23 -0700

    ucomment middleware

--- middleware.ts
+++ middleware.ts
@@ -38,39 +38,39 @@ export async function middleware(req: NextRequest) {
     new RegExp(`^${included.replace(/:\w+/g, '\\w+')}`).test(path)
   )
   console.log("outside the condition", isDynamicPathIncluded)
-  // if (isDynamicPathIncluded && !session) {
-  //   console.log("it works")
-  //   // If user is not authenticated and path is included, redirect to the login page
-  //   if (path.startsWith("/api")) {
-  //     return NextResponse.json(
-  //       { error: "Authorization failed" },
-  //       { status: 403 }
-  //     );
-  //   } else {
-  //     const redirectUrl = new URL("/login", req.url);
-  //     redirectUrl.searchParams.set("redirectedFrom", path);
-  //     return NextResponse.redirect(redirectUrl);
-  //   }
-  // }
+  if (isDynamicPathIncluded && !session) {
+    console.log("it works")
+    // If user is not authenticated and path is included, redirect to the login page
+    if (path.startsWith("/api")) {
+      return NextResponse.json(
+        { error: "Authorization failed" },
+        { status: 403 }
+      );
+    } else {
+      const redirectUrl = new URL("/login", req.url);
+      redirectUrl.searchParams.set("redirectedFrom", path);
+      return NextResponse.redirect(redirectUrl);
+    }
+  }
  // Check if the request path is included in the protected paths
   const isIncludedPath = includedPaths.some((includedPath) =>
     path.startsWith(includedPath)
   );
 
-  // if (isIncludedPath && !session) {
+  if (isIncludedPath && !session) {
     
-  //   // If user is not authenticated and path is included, redirect to the login page
-  //   if (path.startsWith("/api")) {
-  //     return NextResponse.json(
-  //       { error: "Authorization failed" },
-  //       { status: 403 }
-  //     );
-  //   } else {
-  //     const redirectUrl = new URL("/login", req.url);
-  //     redirectUrl.searchParams.set("redirectedFrom", path);
-  //     return NextResponse.redirect(redirectUrl);
-  //   }
-  // }
+    // If user is not authenticated and path is included, redirect to the login page
+    if (path.startsWith("/api")) {
+      return NextResponse.json(
+        { error: "Authorization failed" },
+        { status: 403 }
+      );
+    } else {
+      const redirectUrl = new URL("/login", req.url);
+      redirectUrl.searchParams.set("redirectedFrom", path);
+      return NextResponse.redirect(redirectUrl);
+    }
+  }
 
   // Allow the request to proceed if the user is authenticated or the path is not included
   return res;
